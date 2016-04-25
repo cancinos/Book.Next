03bddd0c84299c2881035d7745d4fdf1f95c0416
Ludwing Cano <ludwing238@gmail.com>
2016-04-24 20:13:19 -0600

    Login
    Creating new Login View

--- BookNext/src/Pages/Login.java
+++ BookNext/src/Pages/Login.java
@@ -7,22 +7,20 @@ package Pages;
 
 import UI.giantCard;
 import UI.Button;
-import UI.VCard;
 import UI.textField;
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
-import com.jfoenix.controls.JFXPopup;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXToggleNode;
 import de.jensd.fx.fontawesome.Icon;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -36,194 +34,119 @@ import javafx.stage.StageStyle;
 public class Login extends Stage{
         
        private Pane page = new Pane();
-       private  giantCard card;
-       private HBox hbox;
-       private VBox vbox;
-       private Label title;
+       private giantCard  card;
+       private TabPane tabPane ;
+       private Tab tab;
        private textField fields;
-       private JFXTextField user;
-       private JFXPasswordField pass;
-       private Button Login;
-       private JFXButton button; 
-       private JFXButton singup; 
-       private JFXToggleNode hamburger;
-       private  giantCard card_u;
-       JFXPopup popup;
+       private VBox logbox;
+       private VBox userbox;
+       private Label title;
+       private ImageView background;
+       private JFXToggleNode icon;
        
-       
-       //This componet creates the card where si going to contain the log in
-    private void  createView(){
+    public void createView(){
         
-        card = new giantCard(400,700);
+        card = new giantCard(400,500);
         card.createCard();        
-        card.setEffect(new DropShadow(10d, 0d, 0d, Color.web("#B6B6B6")));
-        card.setStyle( "-fx-background-color:#F5F5F5;");
-        card.relocate(0, 0);
-    }
-    
-    //Create Login view
-    private void createComponents(){
-        
-        //HBOX contiene el LABEL title
-        hbox = new HBox();
-        hbox.setStyle("-fx-padding-vertical:30; -fx-alignment: left");        
-        hbox.setPadding(new Insets(30, 0, 30, 0));     
-        
-        title = new Label(" LOGIN");
-        title.setStyle("-fx-text-fill: #E91E63; -fx-label-float:true; -fx-font-size:30; -fx-font-weight: BOLD; -fx-padding: 10 0 0 0;" );
-        
-        //VBOX encargado de los TEXTFIELD
-        vbox = new VBox();
-        vbox.setStyle("-fx-padding:30; -fx-alignment: center");
-        vbox.setSpacing(30);
-        vbox.setPrefSize(400,450);
-        
-        fields = new textField();
-        user = fields.validateTextField("Username","User name can't be empty","15");    
-        pass = fields.PasswordField("Password","Password can't be empty","15");
-        Login = new Button("Log in","#FFFFFF","#536DFE");
-        button = Login.getButton();
-        button.setPrefHeight(40);
-        button.setPrefWidth(150);
-       
-        hbox.setPrefSize(400,450);
-        
-        hamburger = HAM("PLUS_CIRCLE","#E91E63");
-	hamburger.setPadding(new Insets(10,5,10,5));
-
-        ImageView background = new ImageView(new Image("https://static-secure.guim.co.uk/sys-images/Observer/Pix/pictures/2013/12/23/1387823611665/Library-with-a-book-ladde-014.jpg"));
-        background.setFitHeight(700);
-        background.setFitWidth(1000);
-        background.relocate(0, 0);
-        
-        page.getChildren().add(background);
-        
-    }
-    
-    //Create new User card
-    private void createUser(){
-      
-       HBox hbox = new HBox();
-        hbox.setStyle("-fx-padding-vertical:30; -fx-alignment: left");        
-        hbox.setPadding(new Insets(30, 0, 30, 0));     
-        
-        //VBOX encargado de los TEXTFIELD
-        VBox vbox = new VBox();
-        vbox.setStyle("-fx-padding:30; -fx-alignment: center");
-        vbox.setSpacing(30);
-        vbox.setPrefSize(400,500);        
-        
-         card_u = new giantCard(400,500);
-         
-         card_u.createCard();
-         card_u.setStyle("-fx-background-color: #E91E63;");
-         card_u.relocate(25, 100);        
-         Label title = new Label("REGISTER");
-         title.setStyle("-fx-text-fill: BLACK; -fx-label-float:true; -fx-font-size:30; -fx-font-weight: BOLD; -fx-padding: 10 0 0 0;" );
-         hbox.getChildren().add(title);       
-        
-         
-          JFXTextField user = fields.validateTextField("Username","User name can't be empty","15");    
-         JFXPasswordField pass = fields.PasswordField("Password","Password can't be empty","15");
-        JFXTextField  name = fields.validateTextField("Name","Please enter your first name","15");    
-         JFXTextField  city = fields.validateTextField("City","Please enter your city","15");    
- 
-        
-        
-        Button Login = new Button("Sing Up","#FFFFFF","#536DFE");
-         button = Login.getButton();
-
-         
-         vbox.getChildren().add(name);
-         vbox.getChildren().add(user);
-         vbox.getChildren().add(city);
-         vbox.getChildren().add(pass);
-         vbox.getChildren().add(button);
-         button.setPrefHeight(40);
-         button.setPrefWidth(150);
-         
-        card_u.getChildren().add(hbox);
-        card_u.getChildren().add(vbox);
-        
-    }
-   
-    //Create icon Toggle
-    private JFXToggleNode HAM(String icon, String color){
-        
-        JFXToggleNode hamburger = new JFXToggleNode();
-        Icon value = new Icon(icon,"6.5em");
-        value.setTextFill(Color.web(color));
-        hamburger.setGraphic(value);
-        hamburger.setStyle("-fx-background-radius: 4em; -fx-background-color:TRANSPARENT;");        
-        hamburger.relocate(275, 550);
-        
-        return hamburger;
-    }
-    
-    //Add all components of Login to page
-    private void addComponents(){
-            
-        hbox.getChildren().add(title);
-        vbox.getChildren().add(user);
-        vbox.getChildren().add(pass);
-        vbox.getChildren().add(button);        
-        card.getChildren().add(hbox);
-        card.getChildren().add(vbox); 
+        card.setEffect(new DropShadow(10d, 5d, 0d, Color.web("#212121")));
+        card.setStyle( "-fx-background-color:#FFFFFF; -fx-background-radius:1em");
+        card.relocate(300, 100);
+        card.getChildren().add(tabPane);        
+        card.getChildren().add(background);
+        card.getChildren().add(icon);
         page.getChildren().add(card);
-	page.getChildren().add(hamburger);
-        
     }
     
-    
-    //Create the events for components
-    private void createEvents(){
-        //Button Login        
-        button.addEventHandler(MouseEvent.MOUSE_PRESSED, (e)->{    
-            button.setStyle("-fx-background-color:#E91E63;"+"-fx-text-fill:WHITE;");
-           });        
-              
-        hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e)->{    
-            changeUser();
-           });        
-     
-         
+    public void createLogin(){
         
+        logbox =new VBox();
+        logbox.setStyle("-fx-alignment: center");
+        logbox.setPadding(new Insets(0,0,0,0));
+        logbox.setSpacing(10);
+        logbox.setPrefSize(400,250);
+        fields = new textField();
+        JFXTextField user = fields.validateTextField("Username","User name can't be empty","14");    
+        JFXPasswordField pass = fields.PasswordField("Password","Password can't be empty","14");
+        logbox.getChildren().add(user);
+        logbox.getChildren().add(pass);
     }
     
-    //Change Card from login to user <->
-    private void changeUser(){
-                
-        if(page.getChildren().contains(card) ==false){
-         page.getChildren().remove(card_u);       
-         page.getChildren().remove(hamburger); 
-         page.getChildren().add(card); 
-        hamburger = HAM("PLUS_CIRCLE","#E91E63");        
-         page.getChildren().add(hamburger); 
-         
-        }else{
-         page.getChildren().remove(card);
-         page.getChildren().remove(hamburger); 
-         page.getChildren().add(card_u); 
-         hamburger = HAM("CHECK_CIRCLE","#FFFFFF"); 
-         hamburger.relocate(325, 500);
-         page.getChildren().add(hamburger);
-        }
-        createEvents();
-    
-        
-        
+    public void createUser(){
+        userbox =new VBox();
+        userbox.setStyle("-fx-alignment: center");
+        logbox.setPadding(new Insets(0,0,0,0));
+        userbox.setSpacing(10);
+        userbox.setPrefSize(400,250);
+        fields = new textField();              
+        JFXTextField name = fields.textField("Name", "14");
+        JFXTextField user = fields.validateTextField("Username","User name can't be empty","14");    
+        JFXPasswordField pass = fields.PasswordField("Password","Password can't be empty","14"); 
+        userbox.getChildren().add(name);
+        userbox.getChildren().add(user);
+        userbox.getChildren().add(pass);
     }
     
+    public void createTabs(){
+      tabPane = new TabPane();
+      tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+      tabPane.getStylesheets().add("/style/jfoenix-components.css");
+      
+      title = new Label("Sing in");
+      title.setTextFill(Color.web("#E91E63"));      
+      title.setStyle("-fx-font-size:15px;");
+      tab = new Tab();      
+      tab.setStyle("-fx-background-color:TRANSPARENT; -fx-alignment:center; -fx-font-weight: bold");
+      tab.setContent(logbox);    
+      tab.setText("");
+      tab.setGraphic(title);
+      
+      tabPane.getTabs().add(tab);
+      
+      title = new Label("Sing up");
+      title.setTextFill(Color.web("#E91E63"));     
+      title.setStyle("-fx-font-size:15px;");
+      tab = new Tab();      
+      tab.setStyle("-fx-background-color:TRANSPARENT; -fx-alignment:center; -fx-font-weight: bold");
+      tab.setContent(userbox);    
+      tab.setText("");  
+      tab.setGraphic(title);
+      
+      tabPane.getTabs().add(tab);
+      tabPane.relocate(0, 200);
+    }
+  
+    public void createPic(){
+       
+        background = new ImageView(new Image("https://static-secure.guim.co.uk/sys-images/Observer/Pix/pictures/2013/12/23/1387823611665/Library-with-a-book-ladde-014.jpg"));
+        background.setFitHeight(200);
+        background.setFitWidth(400);
+        background.relocate(0, 0); 
+        background.setStyle("-fx-background-radius:1em");
+        
+    }   
     
-    //Return stage to show
-    public Stage getStage(){       
-        this.initStyle(StageStyle.UNDECORATED);             
-       createView(); 
-       createComponents();
-       addComponents();
+    public void createToggle(){
+        icon = new JFXToggleNode();
+        Icon value = new Icon("ARROW_RIGHT", "2em");
+        value.setAlignment(Pos.CENTER);
+        value.setTextFill(Color.WHITE);
+        icon.setGraphic(value);
+        icon.setStyle("-fx-background-radius: 4em; -fx-background-color:#E91E63;");
+        icon.setPrefSize(60, 60);
+        icon.relocate(375,415);               
+    }
+    
+    public Stage getStage(){
+        this.initStyle(StageStyle.UNDECORATED);
+       
+       page.setStyle("-fx-background-color:#455A64");
+       createLogin();
        createUser();
-       createEvents();
-       Scene  scene = new Scene(page, 1000, 700);
+       createTabs(); 
+       createPic();
+       createToggle();
+       createView();
+       Scene  scene = new Scene(page, 1000, 700);       
        scene.getStylesheets().add("/style/jfoenix-components.css");
        this.setScene(scene);
        this.setTitle("FXML is Simple");
