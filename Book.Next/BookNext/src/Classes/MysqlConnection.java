0422b9a60dddc645c59f1269dc47770f87b2e4b3
Ludwing Cano <ludwing238@gmail.com>
2016-04-30 09:15:24 -0600

    DataBase
    Connection to DataBase

---
+++ Book.Next/BookNext/src/Classes/MysqlConnection.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Ingesis
+ */
+public class MysqlConnection {
+ 
+    
+    public boolean connect(){
+        
+        
+        return true;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
